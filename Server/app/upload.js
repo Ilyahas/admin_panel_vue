3399038650b9d9f3d8a07cc21eb305b305db155c
ikotlyar <ikotlyar@freshcode.org>
2017-12-28 14:41:42 +0200

    add async await

--- Server/app/upload.js
+++ Server/app/upload.js
@@ -1,55 +1,47 @@
 const multer = require('multer');
 const path = require('path');
 
-const storageDist = multer.diskStorage({
-    destination: function(req, file, callback) {
-        callback(null, '../Client/admin/dist/static/img/photoSectionCovers')
-    },
-    filename: function(req, file, callback) {
-        callback(null, file.originalname)
-    }
-});
-const storageStatic = multer.diskStorage({
-    destination: function(req, file, callback) {
-        callback(null, '../Client/admin/static/img/photoSectionCovers')
-    },
-    filename: function(req, file, callback) {
-        callback(null, file.originalname)
-    }
-});
+const createUploadStorage = (path) => {
+    return multer.diskStorage({
+        destination: function(req, file, callback) {
+            callback(null, path)
+        },
+        filename: function(req, file, callback) {
+            callback(null, file.originalname)
+        }
+    });
+};
 
-exports.uploadSectionCover = (req, res) => {
+const createUploadFunction = (storage) => {
+    return multer({
+        storage: storage,
+        fileFilter: function(req, file, callback) {
+            let ext = path.extname(file.originalname);
+            if (ext !== '.png' && ext !== '.jpg' && ext !== '.svg' && ext !== '.jpeg') {
+                return callback(res.end('Only images are allowed'), null);
+            }
+            callback(null, true);
+        }
+    }).single('file');
+};
+
+const uploadFile = (call, req, res) => {
+    return new Promise(resolve => {
+        call(req, res, (err) => {
+            resolve(!err);
+        });
+    });
+};
+
+exports.uploadSectionCover = async (req, res) => {
     isUploaded = [false, false];
-    let uploadDist = multer({
-        storage: storageDist,
-        fileFilter: function(req, file, callback) {
-            let ext = path.extname(file.originalname);
-            if (ext !== '.png' && ext !== '.jpg' && ext !== '.svg' && ext !== '.jpeg') {
-                return callback(res.end('Only images are allowed'), null);
-            }
-            callback(null, true);
-        }
-    }).single('file');
-    let uploadStatic = multer({
-        storage: storageStatic,
-        fileFilter: function(req, file, callback) {
-            let ext = path.extname(file.originalname);
-            if (ext !== '.png' && ext !== '.jpg' && ext !== '.svg' && ext !== '.jpeg') {
-                return callback(res.end('Only images are allowed'), null);
-            }
-            callback(null, true);
-        }
-    }).single('file');
-    uploadDist(req, res, function(err) {
-        if(err)
-            res.status(400).end("File is not uploaded");
-        isUploaded[0] = true;
-    });
-    uploadStatic(req, res, function(err) {
-        if(err)
-            res.status(400).end("File is not uploaded");
-        isUploaded[1] = true;
-    });
+    let uploadDist = createUploadFunction(createUploadStorage('../Client/admin/dist/static/img/photoSectionCovers'));
+    let uploadStatic = createUploadFunction(createUploadStorage('../Client/admin/static/img/photoSectionCovers'));
+
+    isUploaded[0] = await uploadFile(uploadStatic, req, res);
+    console.log(isUploaded[0]);
+
     // ------------------- change to async await --------------------------- //
-    setTimeout(()=>{if(isUploaded[0] && isUploaded[1]) res.status(200).end("File is uploaded")}, 100);
+    //setTimeout(()=>{if(isUploaded[0] && isUploaded[1]) res.status(200).end("File is uploaded")}, 100);
+
 };
