674f9931647461b292b4b411feb29dd19bf9b82e
IlyaKot <info-site@ukr.net>
2018-02-12 19:25:58 +0200

    img is public

--- app/routes.js
+++ app/routes.js
@@ -20,7 +20,7 @@ module.exports = function(app, path, express, passport) {
 
     app.use('/admin', isLoggedIn, express.static(path.join(newdir_ + '/Client/admin/dist')));
     app.use('/login', isAuthorized, express.static(path.join(newdir_ + '/Client/login/dist')));
-    app.use('/img', isAuthorized, express.static(path.join(newdir_ + '/img')));
+    app.use('/img', express.static(path.join(newdir_ + '/img')));
     app.use('/', express.static(path.join(newdir_ + '/Client/user/dist')));
 
     app.get('/logout', (req, res) => {
