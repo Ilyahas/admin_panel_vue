ddc87db8b49348f902d1a496d6ccd400760e7541
IlyaKot <info-site@ukr.net>
2018-03-31 14:46:02 +0300

    add maps

--- Client/user/src/components/General/View/Contacts.vue
+++ Client/user/src/components/General/View/Contacts.vue
@@ -2,13 +2,33 @@
   <div class="container container-margin">
     <div class="title">{{Contacts.title}}</div>
     <div class="article-content" v-html="Contacts.content"></div>
+
+    <gmap-map
+      :center="center"
+      :zoom="15"
+      style="height: 500px"
+    >
+      <gmap-marker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="center=m.position"
+      ></gmap-marker>
+    </gmap-map>
+
   </div>
 </template>
 <script>
   export default {
     data () {
       return {
-        Contacts: {}
+        Contacts: {},
+        center: {lat: 47.8194042, lng: 35.0524582},
+        markers: [{
+          position: {lat: 47.8197, lng: 35.0515}
+        }]
       }
     },
     methods: {
--- Client/user/src/main.js
+++ Client/user/src/main.js
@@ -15,6 +15,17 @@ import './assets/sass/main.scss'
 import 'swiper/dist/css/swiper.css'
 import 'vuejs-noty/dist/vuejs-noty.css'
 
+import * as VueGoogleMaps from 'vue2-google-maps'
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyDQv6scxuf370EzYaKHIUPPa7qBzhaLT28',
+    libraries: 'places' // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+  }
+})
+
 Vue.use(VueResource)
 Vue.use(Navigation)
 Vue.use(Config)
